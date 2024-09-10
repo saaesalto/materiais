e8a6e3274ba9ff75d50180929de9935f07c2d192
sergiomerces <sergio.merces@gmail.com>
2024-09-10 01:46:26 -0300

    commit inicial

---
+++ js/hidraulico.js
@@ -0,0 +1,12 @@
+const hidraulico = [
+    {
+        codigo: 123456,
+        descricao: 'descrição',
+        unidade: 'unidade',
+        dimensao: '3/4"',
+        cor: 'azul',
+        fornecedor: 'fornecedor',
+        vigencia: 'data',
+        valor: 100.00
+    }
+]
